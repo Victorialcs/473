04fb473ec7c08d8d08a0c71805e2017a31f79f06
Cherie Jiraphanphong <cj5726@princeton.edu>
2024-05-01 21:48:58 -0400

    small chnge 2

--- feedback.py
+++ feedback.py
@@ -1,6 +1,19 @@
 import slither_runner
-import gpt 
+import chatgpt 
 
 def feedback_loop(str user_prompt)
 
     for i in range(0,7):
+        interation = get_chatgpt_response(user_prompt)
+        feedback = run_slither_on_contract(str(interation))
+    
+    return iteration
+
+def main():
+    prompt_example = "Generate an atomic transaction solidity smart contract that allows the user to book a hotel room and airplane ticket at a certain price limit at the same time"
+    final_output = feedback_loop(prompt_example)
+    print(final_output)
+
+
+if __name__ == "__main__":
+    main()
